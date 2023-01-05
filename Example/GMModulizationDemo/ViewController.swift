db2f4fc2f482c53f0fde9df4a3070786e4a1c94e
gumingjian <joegu@Joes-MackBook-pro.local>
2023-01-05 13:40:39 +0800

    修改样例代码

--- Example/GMModulizationDemo/ViewController.swift
+++ Example/GMModulizationDemo/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .buttonText
-        
     }
 
     override func didReceiveMemoryWarning() {
